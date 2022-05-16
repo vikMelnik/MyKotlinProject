f8db889917ee426fa3fb255166c78d5a38a86393
ViktorMel <vitekm@ukr.net>
2022-05-16 15:07:14 +0300

    Testing end

--- app/src/main/java/come/geekbrains/mykotlinproject/MainActivity.kt
+++ app/src/main/java/come/geekbrains/mykotlinproject/MainActivity.kt
@@ -10,6 +10,9 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    private var mFlm: Movie? = null
+    private var mFlm1: Movie? = null
+    private var mFlm2: Movie? = null
 
     private var text_year: TextView? = null
     private var text_director: TextView? = null
@@ -19,10 +22,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_kotlin)
+        setContentView(R.layout.activity_main_kotlin2)
 
-        val flm = Film("Titanik", "Donatello", 2006)
-        val flm2 = flm.copy(auther = "Goblin")
+        mFlm = Movie("Titanic", "Donatello", 2006)
+        mFlm1 = Movie("Fighter", "Quincy", 1994)
+        mFlm2 = mFlm?.copy(director = "Goblin")
+
 
         text_director = findViewById(R.id.text_director)
         text_year = findViewById(R.id.text_year)
@@ -30,12 +35,14 @@ class MainActivity : AppCompatActivity() {
         btn_test = findViewById(R.id.button_test)
 
         btn_test?.setOnClickListener {
-            if (text_field?.text?.toString()?.trim()?.equals("")!!)
+
+            var str = text_field?.text?.toString()
+
+            if (str?.trim()?.equals("")!!)
 
                 Toast.makeText(this, "Enter a word", Toast.LENGTH_SHORT).show()
             else
-
-                Toast.makeText(this, flm2.toString(), Toast.LENGTH_LONG).show()
+                findMovie(str)
 
         }
 
@@ -47,6 +54,26 @@ class MainActivity : AppCompatActivity() {
         /**
          *
          */
+
+    }
+
+    private fun findMovie(s: String) {
+
+        when {
+            s.trim()?.equals("titanic")!! -> {
+                text_year?.text = "Year: " + mFlm?.year.toString()
+                text_director?.text = "Director: " + mFlm?.director.toString()
+
+            }
+            s.trim()?.equals("copy")!! -> {
+                text_year?.text = "Year: " + mFlm2?.year.toString()
+                text_director?.text = "Director: " + mFlm2?.director.toString()
+            }
+            s.trim()?.equals("fighter")!! -> {
+                text_year?.text = "Year: " + mFlm1?.year.toString()
+                text_director?.text = "Director: " + mFlm1?.director.toString()
+            }
+        }
 
     }
 
--- app/src/main/java/come/geekbrains/mykotlinproject/TestKotlin.kt
+++ app/src/main/java/come/geekbrains/mykotlinproject/TestKotlin.kt
@@ -1,13 +1,10 @@
 package come.geekbrains.mykotlinproject
 
-
 fun test() {
-
     for (i in 1..8) {
         println("Films")
     }
 }
-
 
 fun sample() {
     for (i in 10 downTo 1 step 2) {
